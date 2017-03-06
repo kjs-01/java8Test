a6575941325a10b02f21c3fa7692eb7b47a6f074
jake.ko <jake.ko@kakaocorp.com>
2017-03-06 17:26:14 +0900

    master commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,27 +3,5 @@
  */
 public class GitTest {
 
-    //someone2 and js another commit
     //master commit1
-    //master commit2
-    //test1 branch commit1
-    //test1 branch commit2
-
-    //master commit3
-    //master commit4
-
-    //test1 branch commit3
-    //test1 branch commit4
-
-
-    //dummy commit
-    //master commit5
-    //master commit6
-
-    //test1 branch commit3
-    //test1 branch commit4
-
-    //test1 branch commit5
-    //test1 branch commit6
-    //test1 branch commit7
 }
