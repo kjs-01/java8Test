9fbec52b5c3279f3ef961a0b144fd6c58aafeb26
jake.ko <jake.ko@kakaocorp.com>
2017-10-18 20:43:17 +0900

    rollback

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,9 +4,5 @@
 public class GitTest {
 
     //init
-    //test1
-
-    public static void main(String[] args) {
-        System.out.println("args = [" + args + "]");
-    }
 }
+                                                 
